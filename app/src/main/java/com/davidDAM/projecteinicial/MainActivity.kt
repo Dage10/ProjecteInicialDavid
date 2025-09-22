1c5a933675956c1ef9a4858e0b35fa1b57cc9ab8
DavidGelm <david.gelma@institutvidreres.cat>
2025-09-22 14:23:38 +0200

    Projecte Inicial elements Simples

--- app/src/main/java/com/davidDAM/projecteinicial/MainActivity.kt
+++ app/src/main/java/com/davidDAM/projecteinicial/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.davidDAM.projecteinicial
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ViewFlipper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val buttonOk: Button = findViewById(R.id.botoguay);
+        val text : EditText = findViewById(R.id.editText1)
+
+        val vflipper: ViewFlipper = findViewById(R.id.carrousel)
+
+        val imatges = intArrayOf(R.drawable.leon, R.drawable.panda, R.drawable.tiburon)
+
+        for (imatge in imatges) {
+            val imatgeVista = ImageView(this)
+            imatgeVista.setBackgroundResource(imatge)
+            vflipper.addView(imatgeVista)
         }
+
+        buttonOk.setOnClickListener{
+           vflipper.showNext()
+           text.setText("Hola")
+        }
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //    insets
+        //}
     }
 }
